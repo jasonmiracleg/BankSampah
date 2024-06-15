615e7b83a52a54e90809def9f3a25d6f73e73a33
jasonmiracleg <jasonmiracle15@gmail.com>
2024-06-15 20:06:08 +0700

    Transaksi View Done

--- resources/views/Transaksi/create.blade.php
+++ resources/views/Transaksi/create.blade.php
@@ -4,38 +4,38 @@
     <div class="flex items-center justify-center flex-cols h-screen">
         <form class="w-full max-w-md mx-auto" method="POST">
             @csrf
-            <h1 class="mb-4 font-bold text-2xl">Penyetoran Bank Sampah</h1>
+            <h1 class="mb-4 font-bold text-2xl">Pencatatan Transaksi</h1>
             <div class="mb-5">
-                <label for="product_name" class="block mb-2 text-sm font-medium text-gray-900">Nama Barang</label>
-                <input type="text" name="product_name"
+                <label for="keterangan" class="block mb-2 text-sm font-medium text-gray-900">Keterangan</label>
+                <input type="text" name="keterangan"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    placeholder="Masukan Username" required />
-                @error('product_name')
+                    placeholder="Contoh : Hasil Penjualan Bank Sampah" required />
+                @error('keterangan')
                     <span class="mt-2 text-xs text-red-600" role="alert">
                         {{ $message }}
                     </span>
                 @enderror
             </div>
             <div class="mb-5">
-                <label for="weight" class="block mb-2 text-sm font-medium text-gray-900">Berat Barang (Kg)</label>
-                <input type="number" name="weight"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                    placeholder="Masukan Berat Barang" required />
-                @error('weight')
-                    <span class="mt-2 text-xs text-red-600" role="alert">
-                        {{ $message }}
-                    </span>
-                @enderror
-            </div>
-            <div class="mb-5">
-                <label for="recipient" class="block mb-2 text-sm font-medium text-gray-900">Penerima</label>
-                <select name="recipient" id="recipient"
+                <label for="transaction_type" class="block mb-2 text-sm font-medium text-gray-900">Tipe Transaksi</label>
+                <select name="transaction_type" id="transaction_type"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     required>
-                    <option value="" selected>Admin</option>
-                    <option value="Jason">Jason</option>
+                    <option value="" selected>Pemasukan</option>
+                    <option value="Pengeluaran">Pengeluaran</option>
                 </select>
-                @error('weight')
+                @error('transaction_type')
+                    <span class="mt-2 text-xs text-red-600" role="alert">
+                        {{ $message }}
+                    </span>
+                @enderror
+            </div>
+            <div class="mb-5">
+                <label for="total" class="block mb-2 text-sm font-medium text-gray-900">Total Nominal (Rp)</label>
+                <input type="number" name="total"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    placeholder="100000" required />
+                @error('transaction_type')
                     <span class="mt-2 text-xs text-red-600" role="alert">
                         {{ $message }}
                     </span>
@@ -43,7 +43,7 @@
             </div>
             <div class="flex flex-col items-center md:flex-row md:justify-between md:items-center w-full">
                 <button type="submit"
-                    class="text-white bg-green-300 focus:ring-2 focus:outline-none focus:ring-green-400 hover:bg-green-500 font-medium rounded-lg text-sm w-full md:w-auto px-5 py-2.5 text-center">Setor</button>
+                    class="text-white bg-green-300 focus:ring-2 focus:outline-none focus:ring-green-400 hover:bg-green-500 font-medium rounded-lg text-sm w-full md:w-auto px-5 py-2.5 text-center">Simpan Transaksi</button>
             </div>
         </form>
     </div>
--- resources/views/Transaksi/index.blade.php
+++ resources/views/Transaksi/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="max-w-screen-xl mx-auto p-2 mt-2">
         <div class="mt-16 mx-2">
-            <h1 class="font-semibold text-2xl">Riwayat Penyetoran</h1>
+            <h1 class="font-semibold text-2xl">Riwayat Transaksi</h1>
             <hr class="mb-4 border-t border-black">
             <div>
                 <button type="button"
-                    class="focus:outline-none text-white bg-green-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Lakukan
-                    Setor</button>
+                    class="focus:outline-none text-white bg-green-500 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">Buat
+                    Transaksi</button>
             </div>
         </div>
         <div class="mt-4 mx-2">
@@ -19,13 +19,13 @@
                             Tanggal
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Penerima
+                            Keterangan
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Nama Barang
+                            Tipe Transaksi
                         </th>
                         <th scope="col" class="px-6 py-3">
-                            Berat Barang
+                            Total
                         </th>
                     </tr>
                 </thead>
@@ -36,13 +36,13 @@
                             12 Februari
                         </th>
                         <td class="px-6 py-4">
-                            Jason
-                        </td>
-                        <td class="px-6 py-4">
                             Botol Plastik
                         </td>
                         <td class="px-6 py-4">
-                            0.2
+                            Pemasukan
+                        </td>
+                        <td class="px-6 py-4">
+                            Rp 10.000
                         </td>
                     </tr>
                 </tbody>
