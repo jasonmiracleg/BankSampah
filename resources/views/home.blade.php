f6386b96a9cf79e7de119bf550311ab39ce9cd3f
jasonmiracleg <jasonmiracle15@gmail.com>
2024-06-15 14:01:46 +0700

    Home Done

---
+++ resources/views/home.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="max-w-screen-xl mx-auto p-2 mt-2">
+        <div class="md:pt-16 pt-20 flex md:flex-row flex-col w-full">
+            <div class="w-full p-2">
+                <a class="block bg-blue-500 text-white rounded-lg shadow mb-4 no-underline">
+                    <div class="p-4">
+                        <h2 class="font-bold">Saldo</h2>
+                        <hr class="border-t-2 my-1">
+                        <h3 class="font-semibold">Rp 17.000</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="w-full p-2">
+                <a class="block bg-blue-500 text-white rounded-lg shadow mb-4 no-underline">
+                    <div class="p-4">
+                        <h2 class="font-bold">Pemasukan</h2>
+                        <hr class="border-t-2 my-1">
+                        <h3 class="font-semibold">Rp 17.000</h3>
+                    </div>
+                </a>
+            </div>
+            <div class="w-full p-2">
+                <a class="block bg-blue-500 text-white rounded-lg shadow mb-4 no-underline">
+                    <div class="p-4">
+                        <h2 class="font-bold">Pengeluaran</h2>
+                        <hr class="border-t-2 my-1">
+                        <h3 class="font-semibold">Rp 17.000</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="px-2">
+            <h1 class="font-semibold text-2xl">Transaksi Terakhir</h1>
+            <hr class="mb-4 border-t border-black">
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-700">
+                    <thead class="text-xs text-gray-800 uppercase bg-green-300">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Tanggal
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Keterangan
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Tipe Transaksi
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Total
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr
+                            class="odd:bg-white even:bg-gray-100 dark:odd:bg-gray-900 dark:even:bg-gray-800 border-b dark:border-gray-700">
+                            <th scope="row"
+                                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                Apple MacBook Pro 17"
+                            </th>
+                            <td class="px-6 py-4">
+                                Silver
+                            </td>
+                            <td class="px-6 py-4">
+                                Laptop
+                            </td>
+                            <td class="px-6 py-4">
+                                $2999
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,10 @@
 </head>
 
 <body>
-    <nav class="bg-white border-gray-200 top-0 fixed z-50 w-full">
+    <nav class="bg-green-500 border-gray-200 top-0 fixed z-50 w-full">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap">Bank Sampah</span>
+                <span class="self-center text-2xl text-white font-semibold whitespace-nowrap">Bank Sampah</span>
             </a>
             <button data-collapse-toggle="navbar-default" type="button"
                 class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
@@ -29,27 +28,42 @@
             </button>
             <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul
-                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
+                    class="navbar-navfont-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-transparent">
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
-                            aria-current="page">Home</a>
+                            class="block py-2 px-3 md:text-white bg-blue-700 rounded font-semibold md:bg-transparent md:p-0 nav-link md:hover:text-gray-200">Beranda</a>
                     </li>
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">About</a>
+                            class="block py-2 px-3 md:text-white rounded hover:bg-gray-100 font-semibold md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 nav-link">Penyetoran</a>
                     </li>
                     <li>
                         <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Services</a>
+                            class="block py-2 px-3 md:text-white rounded hover:bg-gray-100 font-semibold md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 nav-link">Transaksi</a>
                     </li>
                     <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Pricing</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0">Contact</a>
+                        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar"
+                            class="flex items-center justify-between w-full py-2 px-3 md:text-white font-semibold rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-200 md:p-0 md:w-auto">Nama Orang
+                            <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                fill="none" viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="m1 1 4 4 4-4" />
+                            </svg></button>
+                        <!-- Dropdown menu -->
+                        <div id="dropdownNavbar"
+                            class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-400"
+                                aria-labelledby="dropdownLargeButton">
+                                <li>
+                                    <a href="#"
+                                        class="block px-4 py-2 hover:bg-gray-100">Profil</a>
+                                </li>
+                            </ul>
+                            <div class="py-1">
+                                <a href="#"
+                                    class="block px-4 py-2 text-sm text-gray-700">Logout</a>
+                            </div>
+                        </div>
                     </li>
                 </ul>
             </div>
