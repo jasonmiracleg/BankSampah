50278e179d3ae41324cf0ab66e3ff82892f559e6
jasonmiracleg <jasonmiracle15@gmail.com>
2024-06-24 05:36:57 +0700

    Sign Up Admin Only

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -44,7 +44,6 @@
             <div class="flex flex-col items-center md:flex-row md:justify-between md:items-center w-full">
                 <button type="submit"
                     class="text-white bg-green-300 focus:ring-2 focus:outline-none focus:ring-green-400 hover:bg-green-500 font-medium rounded-lg text-sm w-full md:w-auto px-5 py-2.5 text-center">Daftar</button>
-                <p class="mt-2 md:mt-0"><a href="{{ route('login.page') }}" class="text-blue-600 underline">Sudah Punya Akun?</a></p>
             </div>            
         </form>
     </div>
